c72cef35a8ba70718af8b7fc9f6e1f4fa502369f
ribhuhooja <ribhu.hooja.27@dartmouth.edu>
2024-07-01 10:57:03 -0400

    more problems in sec2

--- ch2/sec2.rkt
+++ ch2/sec2.rkt
@@ -8,14 +8,11 @@
 
 ;;2.18
 (define (reverse items)
-  (if (null? items)
-      nil
-      (cons (last-pair items) (reverse (until-second-last items)))))
-
-(define (until-second-last items)
-  (if (null? (cdr items))
-      nil
-      (cons (car items) (until-second-last (cdr items)))))
+  (define (collect items accd)
+    (if (null? items)
+        accd
+        (collect (cdr items) (cons (car items) accd))))
+  (collect items nil))
 
 ;;skipping 2.19 -- Also, the above solution for 2.18 is probably pretty inefficient
 
@@ -23,13 +20,53 @@
 (define (same-parity x . items)
   (define (same-parity? x y)
     (= 0 (remainder (- x y) 2)))
+  (define (collect items)
+    (cond ((null? items) nil)
+          ((same-parity? x (car items))
+           (cons (car items) (collect (cdr items))))
+          (else (collect (cdr items)))))
+  (cons x (collect items)))
+
+;2.21
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (* (car items) (car items)) (square-list (cdr items)))))
+
+(define (map-square-list items)
+  (map (lambda (x) (* x x)) items))
+
+;2.22
+(define (foreach proc items)
+  (cond ((null? items) true)
+        (else (proc (car items))
+              (foreach proc (cdr items)))))
+
+;------------------------ thus ends 2.2.1 ----------------------------
+;2.24 - pointless
+;2.25 - also pointless
+;2.26 - also also pointless
+;2.27
+(define (deep-reverse items)
   (define (collect items accd)
     (cond ((null? items) accd)
-          ((same-parity? x (car items))
-           (collect (cdr items) (cons (car items) accd)))
-          (else (collect (cdr items) accd))))
+          ((not (pair? items))
+           (if (null? accd)
+               items
+               (cons items accd)))
+          (else (collect (cdr items)
+                         (cons (deep-reverse (car items)) accd)))))
   (collect items nil))
 
-                                    
+;2.28
+(define (fringe tree)
+  (define (collect tree accd)
+    (cond ((null? tree) accd)
+          ((not (pair? tree)) (append accd (cons tree nil)))
+          (else (collect (cdr tree) (collect (car tree) accd)))))
+  (collect tree nil))
+          
+;2.29
+
+
         
-    
